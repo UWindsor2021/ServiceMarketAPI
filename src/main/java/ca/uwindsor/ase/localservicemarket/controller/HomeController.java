647f9098de80a3f998bd09bc4e46ddd19b328616
krsays <kalyanramthandu2@gmail.com>
2021-10-21 12:41:32 -0400

    Testing Connection

--- src/main/java/ca/uwindsor/ase/localservicemarket/controller/HomeController.java
+++ src/main/java/ca/uwindsor/ase/localservicemarket/controller/HomeController.java
@@ -1,7 +1,10 @@
 package ca.uwindsor.ase.localservicemarket.controller;
 
 
+import ca.uwindsor.ase.localservicemarket.dto.ServiceSeeker;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +13,15 @@ public class HomeController {
     public String index() {
         return "<h1>Welcome!</h1>";
     }
+
+/*    @GetMapping("/getDetails")
+    public ServiceSeeker getServiceSeekerDetails(@RequestParam Long id){
+        ServiceSeeker s = new ServiceSeeker();
+        ServiceSeeker seeker = s;
+        seeker.setId(id);
+        seeker.setTitle("Hello Kalyan");
+        return seeker;
+
+    }*/
+    //Checking Connection
 }
